97e9fee67543d94abf1d1af9cdf5b33fc9b82eee
gajendra <davegk59@gmil.com>
2023-11-21 15:06:33 +0530

    added employee management

--- emp/urls.py
+++ emp/urls.py
@@ -4,5 +4,8 @@ from . views import  *
 
 urlpatterns = [
     
-    path('emp/',employee),
+    path('home/',employee),
+    path('add-emp/',add_emp),
+    path('delete-emp/<int:emp_id>',delete_emp),
+    path('update-emp/<int:emp_id>',update_emp),
 ]
--- emp/views.py
+++ emp/views.py
@@ -1,6 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect 
+from django.http import HttpResponse 
 from .models import Emp 
 
+from .forms import EmpForm 
 def employee(request):
     emps = Emp.objects.all()
-    return render(request,"employee.html",{'emps':emps})
+    return render(request,"emp/home.html",{'emp':emps})
+
+
+def add_emp(request):
+    if request.method == "POST" :
+        print('data is commitng')
+        
+        #data fetch 
+        emp_name = request.POST.get("emp_name")
+        emp_id = request.POST.get('emp_id')
+        emp_phone = request.POST.get('emp_phone')
+        emp_address = request.POST.get("emp_address")
+        # emp_working = request.POST.get('emp_working')
+        emp_department = request.POST.get("emp_department")
+        
+        
+        #Validation
+        
+        #create model object and set the data
+        
+        e = Emp()
+        e.name = emp_name
+        e.emp_id = emp_id
+        e.phone = emp_phone
+        e.address = emp_address
+        e.working = emp_working
+        e.department = emp_department
+        
+        if emp_working is None:
+            e.working=False
+        else:
+            e.working=True
+            
+        # Save the object 
+        e.save()
+        
+        #prepare msg
+        
+        return redirect("/emp/home/")
+    
+    form=EmpForm()
+    
+    return render(request,'emp/add_emp.html')
+
+def delete_emp(request, emp_id):
+    print(emp_id)
+    emp = Emp.objects.get(pk=emp_id)
+    emp.delete()
+    return redirect("/emp/home/") 
+
+def update_emp(request, emp_id):
+    emp=Emp.objects.get(pk=emp_id)
+    return render(request,"emp/update_emp.html",{"emp":emp })
+    
